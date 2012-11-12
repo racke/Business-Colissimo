6f655d2c5909a046009612c4981e7732195f8cb8
Stefan Hornburg (Racke) <racke@linuxia.de>
2012-11-12 08:47:59 +0100

    Bump up version number.

--- lib/Business/Colissimo.pm
+++ lib/Business/Colissimo.pm
@@ -12,11 +12,11 @@ Business::Colissimo - Shipping labels for ColiPoste
 
 =head1 VERSION
 
-Version 0.2100
+Version 0.2200
 
 =cut
 
-our $VERSION = '0.2100';
+our $VERSION = '0.2200';
 
 my %product_codes = (access_f => '8L', 
 		     expert_f => '8V', 
